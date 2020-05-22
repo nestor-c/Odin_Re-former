aef2f8078e67196ce8deff3c772df2acbc05eaa2
nestor-c <nestord.cortes@gmail.com>
2020-05-21 19:39:51 -0700

    Finished adding validations and displaying any error messages in for the user model

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -2,18 +2,31 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+    def show
+        @user = User.find(params[:id])
+    end
     def create
         #@user = User.new(username: params[:username], email: params[:email], password:params[:password])
         @user = User.new(user_params)
         if @user.save
-            redirect_to new_user_path
+            redirect_to user_path(@user)
         else 
             render :new
         end
     end
+    def edit
+        @user = User.find(params[:id]);
+    end
+    
+
+    def update 
+        @user = User.find(params[:id]);
+        @user.update(user_params);
+        redirect_to user_path(@user)
+    end
     private
     def user_params
-        params.require(:user).permit(:username, :password, :email)
+        params.require(:user).permit(:username, :password, :email, :errors)
     end
 
 end
